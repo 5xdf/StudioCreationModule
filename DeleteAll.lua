e7431e0ec52218635d0c4d4ec79f89dc6fd05a3f
PerplexDemon <89792698+PerplexDemon@users.noreply.github.com>
2022-04-18 11:33:10 -0400

    Create DeleteAll.lua

---
+++ DeleteAll.lua
@@ -0,0 +1,8 @@
+for _,v in pairs(game.Workspace:GetChildren()) do
+  v.Anchored = false
+  v:Destroy()
+ end
+-- set the disabled value to true when inserted
+
+-- Made by nbur21/PerplexDemon
+-- Do not attempt to reupload as your own.
